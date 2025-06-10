19afaa8308ee30060e1b615916bc5e17fa092e5f
tejas-a-rane <142714682+tejas-a-rane@users.noreply.github.com>
2025-06-10 18:48:07 +0530

    Update Q5.js

--- test/Q5.js
+++ test/Q5.js
@@ -1,11 +1,13 @@
 const arr = [10,20,30,60,80,90]
 
 const map = arr.map((arr)=>{
-    return arr*2
+    if(arr>90){return "A"}
+    else if(arr>70){return "B"}
+    else return "C" 
 })
 
 const filter = arr.filter((arr)=>{
-    return arr > 50
+    return arr > 75
 })
 const reduce = arr.reduce((total,num)=>{
     return total + num
