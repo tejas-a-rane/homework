a587f905b7a5cd71963bc8b49350ca5c45bf963a
tejas-a-rane <142714682+tejas-a-rane@users.noreply.github.com>
2025-06-17 19:55:09 +0530

    Update Q07.js

--- c2/Q07.js
+++ c2/Q07.js
@@ -1,21 +1,18 @@
 let stock = 5
 
-function meth(amtreq){
+function checkStock(amtreq){
     if(stock<amtreq){
         throw new Error("Item quantity unavailable")
     }
 }
 
-function meth2(){
-    meth(6)
+function addToCart(){
+    checkStock(6)
 }
 
-function meth3(){
-    meth2()
-}
 
 try {
-    meth3()
+    addToCart()
 } catch (error) {
     console.log(error.name +":"+error.message);
     
